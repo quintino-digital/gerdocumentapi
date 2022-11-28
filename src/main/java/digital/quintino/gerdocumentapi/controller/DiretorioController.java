0bff6e6eab4bc4348c9adc4526292215c4eaaaba
Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-11-27 21:49:48 -0300

    feature: Modelagem da tabela TB_DIRETORIO

---
+++ src/main/java/digital/quintino/gerdocumentapi/controller/DiretorioController.java
@@ -0,0 +1,28 @@
+package digital.quintino.gerdocumentapi.controller;
+
+import digital.quintino.gerdocumentapi.domain.DiretorioDomain;
+import digital.quintino.gerdocumentapi.service.DiretorioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/diretorio")
+public class DiretorioController {
+	
+	@Autowired
+	private DiretorioService diretorioService;
+
+	@PostMapping
+	public ResponseEntity<DiretorioDomain> saveOne(@RequestBody DiretorioDomain diretorioDomain) {
+		return ResponseEntity.ok().body(this.diretorioService.saveOne(diretorioDomain));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<DiretorioDomain>> findAll() {
+		return ResponseEntity.ok(this.diretorioService.findAll());
+	}
+
+}
